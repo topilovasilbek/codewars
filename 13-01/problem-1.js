3c68af80d192cc2925c2ef52a9b8c086481ced72
topilovasilbek <asilbektback@gmail.com>
2023-01-13 20:35:12 +0500

    6 kyu - Ho Ho Ho with Functions!

---
+++ 13-01/problem-1.js
@@ -0,0 +1,9 @@
+// 6 kyu - Ho Ho Ho with Functions!
+
+function ho(param) {
+  return param ? "Ho " + param : "Ho!";
+}
+
+console.log(ho()); // should return "Ho!"
+// console.log(ho(ho())); // should return "Ho Ho!"
+// console.log( ho(ho(ho())) ); // should return "Ho Ho Ho!"
