2142ef52c6fa56b2ac828b0ae25207b0580cc05c
topilovasilbek <asilbektback@gmail.com>
2023-05-08 18:11:14 +0500

    6 kyu - Array.isArray

---
+++ 08-05/problem-1.ts
@@ -0,0 +1,19 @@
+// 6 kyu - Array.isArray
+
+function isArray(arr: any) {
+  // This is what we would like to do, but it's not allowed.
+  return Object.prototype.toString.call(arr) === "[object Array]";
+}
+
+console.log(isArray([])); // true
+// console.log(isArray({})); // false
+// console.log(isArray(null)); // false
+// console.log(isArray(undefined)); // false
+// console.log(isArray({ length: 0 })); // false
+// console.log(isArray("Array")); // false
+// console.log(isArray("[]")); // false
+// console.log(isArray("[1, 2]")); // false
+// console.log(isArray(Array)); // false
+// console.log(isArray(Array())); // true
+// console.log(isArray(Array.prototype)); // true
+// console.log(isArray(Object.defineProperty([], "prop", { value: "value" }))); // true
