bcc6fafc1fd6e97d1060bffa9a7f4245b6aa71d2
topilovasilbek <asilbektback@gmail.com>
2023-06-16 14:49:31 +0500

    6 kyu - not So Easy: Count characters

---
+++ 16-06/problem-1.js
@@ -0,0 +1,18 @@
+// 6 kyu - not So Easy: Count characters
+var inputLength = null;
+var originConsoleLog = console.log;
+console.log = function (input) {
+    inputLength = input === null || input === void 0 ? void 0 : input.length;
+    return originConsoleLog.apply(null, arguments);
+};
+function count() {
+    return inputLength;
+}
+console.log("1");
+console.log(count()); // 1
+console.log("aa");
+console.log(count()); // 2
+console.log("abc");
+console.log(count()); // 3
+console.log("abcdefghijklmn");
+console.log(count()); // 14
